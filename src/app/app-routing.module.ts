4c2c8a6fe5cecf5d06c6288d5b4d0834957f5f1b
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-07-08 13:22:28 -0400

    checking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,15 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+//step 2 is to  import all of the components you will be using in the <router-module> tag
 
 
 
 
 
-
+//step 3 is define the Routes
+// a route entry takes the form of
+// {}
 const appRoutes: Routes = [
-  
+
 ];
