591182e22c2ba4e9debc40556ffe7964182c9e20
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-07-11 21:15:10 -0400

    this is a really good example of the router service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,14 +14,20 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  constructor(private recService: RecipeService) { }
+  constructor(private recService: RecipeService,
+              private routerService: Router,
+              private route: ActivatedRoute) { }
 
-
+//activated route will inf orm us of the current route
 
   ngOnInit() {
     this.recipes = this.recService.getRecipes();
   }
 
-
+  onNewRecipe(){
+    //in this commit we learn how to use the routerservice module to navigate in the type script code
+    //fo rthe purposes ofrelative file path navigation
+    this.routerService.navigate(['new'], { relativeTo: this.route})
+  }
 
 }
